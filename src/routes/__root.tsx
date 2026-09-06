6f3e8bcb92ea943bec9ad1edbce25ec7d75cf587
Midosd249 <ahmed_mahmoud_eng_Sd@hotmail.com>
2026-09-06 03:07:44 +0300

    feat: load Heritage theme stylesheet

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,6 +10,7 @@ import noirThemeCss from "../theme-noir.css?url";
 import themeRefinementsCss from "../theme-refinements.css?url";
 import themeRefinementsV2Css from "../theme-refinements-v2.css?url";
 import editorialThemeCss from "../theme-editorial.css?url";
+import heritageThemeCss from "../theme-heritage.css?url";
 import menuPreviewLayerCss from "../menu-preview-layer.css?url";
 
 const APP_NAME = "منيو";
@@ -37,6 +38,7 @@ export const Route = createRootRoute({
       { rel: "stylesheet", href: themeRefinementsCss },
       { rel: "stylesheet", href: themeRefinementsV2Css },
       { rel: "stylesheet", href: editorialThemeCss },
+      { rel: "stylesheet", href: heritageThemeCss },
       { rel: "stylesheet", href: menuPreviewLayerCss },
       { rel: "manifest", href: "/__grok/manifest.webmanifest" },
       { rel: "apple-touch-icon", href: "/__grok/icon-180.png" },
