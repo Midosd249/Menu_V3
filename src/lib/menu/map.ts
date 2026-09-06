e6c7f7d5faffa036d2d13d0e90df774ff9c4fe93
Midosd249 <ahmed_mahmoud_eng_Sd@hotmail.com>
2026-09-06 21:08:36 +0300

    fix: use explicit ESM paths for menu mapper test compatibility

--- src/lib/menu/map.ts
+++ src/lib/menu/map.ts
@@ -1,5 +1,5 @@
-import { isThemeKey, DEFAULT_THEME_KEY } from "../theme";
-import { bool, num } from "../utils";
+import { isThemeKey, DEFAULT_THEME_KEY } from "../theme/index.ts";
+import { bool, num } from "../utils.ts";
 import type { Branch, BranchHour, Category, Product, PublicTenant, Tenant } from "./types";
 
 function str(value: unknown): string {
