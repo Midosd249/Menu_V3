11c61789f16a4ac9435c6463e4362d980f831904
Midosd249 <ahmed_mahmoud_eng_Sd@hotmail.com>
2026-09-15 04:58:20 +0300

    feat(w7.9): add Admin overview route

---
+++ src/routes/admin/index.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { PlatformAdminPage } from "@/routes/admin";
+
+export const Route = createFileRoute("/admin/")({
+  component: PlatformAdminOverviewRoute,
+});
+
+function PlatformAdminOverviewRoute() {
+  return <PlatformAdminPage initialTab="overview" />;
+}
