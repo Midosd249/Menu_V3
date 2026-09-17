80e0cc898931adebf95376d1516a741973aca94f
Midosd249 <ahmed_mahmoud_eng_Sd@hotmail.com>
2026-09-17 19:14:19 +0300

    fix: remove redundant admin route adapter

--- src/routes/admin/branches.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { PlatformAdminPage } from "@/routes/admin";
-
-export const Route = createFileRoute("/admin/branches")({
-  component: () => <PlatformAdminPage initialTab="branches" />,
-});
